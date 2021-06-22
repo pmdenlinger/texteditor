ce8b0fb2a8d1ed204f8b41972947cf84db21171a
Paul Denlinger <pmdenlinger@icloud.com>
2021-06-21 17:16:09 -0700

    Initial Commit

---
+++ texteditor/texteditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  texteditorApp.swift
+//  texteditor
+//
+//  Created by user on 6/21/21.
+//
+
+import SwiftUI
+
+@main
+struct texteditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
